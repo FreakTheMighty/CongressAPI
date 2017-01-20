eb2800ecc798e033bb1590d4e31ce32b80b9a718
Jesse Vander Does <jesse@htmlfusion.com>
2017-01-19 22:14:12 -0800

    Temp removing xml to json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var request = require('request');
-var parser = require('xml2json');
+//var parser = require('xml2json');
 
 var CONGRESS_XML = 'http://www.senate.gov/general/contact_information/senators_cfm.xml';
 
@@ -9,9 +9,9 @@ app.get('/', function (req, res) {
 
   request(CONGRESS_XML, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      var json = parser.toJson(body);
-      res.setHeader('Content-Type', 'application/json');
-      res.send(json);
+      //var json = parser.toJson(body);
+      res.setHeader('Content-Type', 'application/xml');
+      res.send(body);
     }
   });
 });
