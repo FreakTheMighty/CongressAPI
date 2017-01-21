aaacc804ab8a3a245244c14cc9e1d09fd9fc0b30
Jesse Vander Does <jesse@htmlfusion.com>
2017-01-21 12:15:12 -0800

    Bumping cache key

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ module['exports'] = function convert(hook) {
     WY: 'Wyoming'
   };
 
-  var CACHE_KEY = 'contacts';
+  var CACHE_KEY = 'contacts:1';
 
   var SENATE_API = 'http://www.senate.gov/general/contact_information/senators_cfm.xml';
 
